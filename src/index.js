fe98256f0209433d0f0961c11e7b7d2649d6e3fc
lorengreenfield <loren@intereact.co.nz>
2017-05-14 09:23:12 +1200

    Changed route config to just an initial global halfcab config

--- src/index.js
+++ src/index.js
@@ -2,39 +2,12 @@ import observer from '@nx-js/observer-util';
 import sheetRouter from 'sheet-router';
 import href from 'sheet-router/href';
 import html, {update} from 'yo-yo';
+import { get } from 'axios';
 var routesArray = [];
+var baseApiPath = '';
+var store;
 
-var store = observer.observable({});
 
-function routes(config){
-
-    observer.observe(() => update(config.rootComponent, config.components()));
-
-    var routesFormatted = routesArray.map(route => [
-        route.path,
-        (params) =>{
-            route.callback(params);
-            update(config.rootComponent, config.components());
-            if(window.location.pathname !== route.path){
-                window.history.pushState({path: route.path}, route.title, route.path);
-                document.title = route.path !== '' ? `${config.baseName} - ${route.title}`: config.baseName;
-            }
-        }
-
-    ]);
-
-    var router = sheetRouter({default: '/404'}, routesFormatted);
-
-    href((link) =>{
-        router(link.pathname);
-    });
-
-    window.onpopstate = function(event) {
-        router(event.state && event.state.route || '/');
-    };
-
-    return router;
-}
 
 function route(routeObject, callback){
     routesArray.push(Object.assign(routeObject, {callback}));
@@ -53,10 +26,48 @@ function formField(ob, prop){
     }
 }
 
-export default function(){
+
+export default function (config){
+
+    store = observer.observable(config.store || {});
+
+    baseApiPath = config.baseApiPath || '';
+    observer.observe(() => update(config.rootComponent, config.components()));
+
+    var routesFormatted = routesArray.map(route => [
+        route.path,
+        (params) =>{
+
+            //get data that the route needs first
+            get(`${baseApiPath}${config.path}`)
+                .then(data => {
+                    config.data = data.data;
+                    route.callback(params);
+                    update(config.rootComponent, config.components());
+                    if(window.location.pathname !== route.path){
+                        window.history.pushState({path: route.path}, route.title, route.path);
+                        document.title = route.path !== '' ? `${config.baseName} - ${route.title}`: config.baseName;
+                    }
+                })
+                .catch(err => {
+                    console.log(err.toString());
+                });
+        }
+
+    ]);
+
+    var router = sheetRouter({default: '/404'}, routesFormatted);
+
+    href((link) =>{
+        router(link.pathname);
+    });
+
+    window.onpopstate = function(event) {
+        router(event.state && event.state.route || '/');
+    };
+
     return {
         html,
-        routes,
         route,
         store,
         emptyBody,
@@ -64,4 +75,5 @@ export default function(){
     }
 }
 
-export {html, routes, route, store, emptyBody, formField};
+
+export {html, route, store, emptyBody, formField};
