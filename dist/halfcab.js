6ba6067cf219d379893e27449a8642c2ee90d5f6
lorengreenfield <loren@intereact.co.nz>
2017-07-16 08:23:29 +1200

    Prevent geb from running during SSR

--- dist/halfcab.js
+++ dist/halfcab.js
@@ -21,37 +21,49 @@ var events = ee({});
 
 function eventEmitter(){
 
-    function broadcast(eventName, eventObject){
+    var noop = () => {};
 
-        //Set a break point on the following line to monitor all events being broadcast
-        console.log('Event broadcast: '+ eventName);
-        events.emit(eventName, eventObject);
+    if(typeof window !== 'undefined'){
+        function broadcast(eventName, eventObject){
+
+            //Set a break point on the following line to monitor all events being broadcast
+            console.log('Event broadcast: '+ eventName);
+            events.emit(eventName, eventObject);
+        }
+
+        function on(eventName, cb){
+
+            //Set a break point on the following line to monitor all events being listened to
+            events.on(eventName, cb);
+        }
+
+        function once(eventName, cb){
+
+            //Set a break point on the following line to monitor all events being listened to just once
+            events.once(eventName, cb);
+        }
+
+        function off(eventName, listenerFunction){
+
+            //Set a break point on the following line to monitor all events being unlistened to
+            events.off(eventName, listenerFunction);
+        }
+
+        return {
+            broadcast: broadcast,
+            on: on,
+            once: once,
+            off: off
+        }
+    }else{
+        return {
+            broadcast: noop,
+            on: noop,
+            once: noop,
+            off: noop
+        }
     }
 
-    function on(eventName, cb){
-
-        //Set a break point on the following line to monitor all events being listened to
-        events.on(eventName, cb);
-    }
-
-    function once(eventName, cb){
-
-        //Set a break point on the following line to monitor all events being listened to just once
-        events.once(eventName, cb);
-    }
-
-    function off(eventName, listenerFunction){
-
-        //Set a break point on the following line to monitor all events being unlistened to
-        events.off(eventName, listenerFunction);
-    }
-
-    return {
-        broadcast: broadcast,
-        on: on,
-        once: once,
-        off: off
-    }
 }
 
 var eventEmitter$1 = new eventEmitter();
