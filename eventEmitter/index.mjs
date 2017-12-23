cdb39a4f92af11d71b9a7952ef073f5426e1d16a
lorengreenfield <loren@intereact.co.nz>
2017-12-23 21:29:40 +1300

    Fixed event emitter test and nyc using mjs files

--- eventEmitter/index.mjs
+++ eventEmitter/index.mjs
@@ -1,50 +1,50 @@
-import ee from 'event-emitter'
-var events = ee({})
+import eeModule from 'event-emitter'
 
-function eventEmitter(){
+function eventEmitter ({ee = eeModule} = {}) {
 
-    var noop = () => {}
+  var events = ee({})
+  var noop = () => {}
 
-    if(typeof window !== 'undefined'){
-        function broadcast(eventName, eventObject){
+  if (typeof window !== 'undefined') {
+    function broadcast (eventName, eventObject) {
 
-            //Set a break point on the following line to monitor all events being broadcast
-            console.log('Event broadcast: '+ eventName)
-            events.emit(eventName, eventObject)
-        }
-
-        function on(eventName, cb){
-
-            //Set a break point on the following line to monitor all events being listened to
-            events.on(eventName, cb)
-        }
-
-        function once(eventName, cb){
-
-            //Set a break point on the following line to monitor all events being listened to just once
-            events.once(eventName, cb)
-        }
-
-        function off(eventName, listenerFunction){
-
-            //Set a break point on the following line to monitor all events being unlistened to
-            events.off(eventName, listenerFunction)
-        }
-
-        return {
-            broadcast: broadcast,
-            on: on,
-            once: once,
-            off: off
-        }
-    }else{
-        return {
-            broadcast: noop,
-            on: noop,
-            once: noop,
-            off: noop
-        }
+      //Set a break point on the following line to monitor all events being broadcast
+      console.log('Event broadcast: ' + eventName)
+      events.emit(eventName, eventObject)
     }
+
+    function on (eventName, cb) {
+
+      //Set a break point on the following line to monitor all events being listened to
+      events.on(eventName, cb)
+    }
+
+    function once (eventName, cb) {
+
+      //Set a break point on the following line to monitor all events being listened to just once
+      events.once(eventName, cb)
+    }
+
+    function off (eventName, listenerFunction) {
+
+      //Set a break point on the following line to monitor all events being unlistened to
+      events.off(eventName, listenerFunction)
+    }
+
+    return {
+      broadcast: broadcast,
+      on: on,
+      once: once,
+      off: off
+    }
+  } else {
+    return {
+      broadcast: noop,
+      on: noop,
+      once: noop,
+      off: noop
+    }
+  }
 
 }
 
--- eventEmitter/test.mjs
+++ eventEmitter/test.mjs
@@ -1,106 +1,108 @@
-import chai, {expect} from 'chai';
-import dirtyChai from 'dirty-chai';
+import chai from 'chai'
+import dirtyChai from 'dirty-chai'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
-import { noPreserveCache } from 'proxyquire';
-const proxyquire = noPreserveCache().noCallThru();
-chai.use(dirtyChai);
-chai.use(sinonChai);
+import { eventEmitter } from './index'
+import jsdomGlobal from 'jsdom-global'
 
-var mocks = {
-    emit: sinon.stub(),
-    on: sinon.stub(),
-    once: sinon.stub(),
-    off: sinon.stub()
+const {expect} = chai
+chai.use(dirtyChai)
+chai.use(sinonChai)
+let jsdom = jsdomGlobal()
+let mocks = {
+  emit: sinon.stub(),
+  on: sinon.stub(),
+  once: sinon.stub(),
+  off: sinon.stub()
 }
 
-global.window = {}
-var moduleUnderTest = proxyquire('./index.js', {
-    'event-emitter'(){
-        return mocks
-    }
-}).default
+let ee = () => {
+  return mocks
+}
 
 
+let geb = new eventEmitter({ee})
 
-import geb, { eventEmitter } from './index'
+describe('eventEmitter', function () {
 
-describe('eventEmitter', function(){
+  before(function () {
+    return new Promise(resolve => {
 
-    before(function(){
-        return new Promise(resolve => {
-
-            resolve()
-        })
-
-    });
-
-    describe('client', function(){
-
-        it('calls broadcast correctly', (done) => {
-            moduleUnderTest.broadcast('some event', {})
-            expect(mocks.emit).to.have.been.called()
-            mocks.emit.reset()
-            done()
-        })
-
-        it('calls on correctly', (done) => {
-            moduleUnderTest.on('some event', () => {})
-            expect(mocks.on).to.have.been.called()
-            mocks.on.reset()
-            done()
-        })
-
-        it('calls once correctly', (done) => {
-            moduleUnderTest.once('some event', () => {})
-            expect(mocks.once).to.have.been.called()
-            mocks.once.reset()
-            done()
-        })
-
-        it('calls off correctly', (done) => {
-            moduleUnderTest.off('some event', () => {})
-            expect(mocks.off).to.have.been.called()
-            mocks.off.reset()
-            done()
-        })
+      resolve()
     })
 
+  })
 
-    describe('server', function(){
-        return new Promise(resolve => {
-            it('calls a noop for broadcast', () => {
-                global.window = undefined
-                var emitter = new eventEmitter()
+  describe('client', function () {
 
-                emitter.broadcast('some new event')
-                expect(mocks.emit).not.to.have.been.called()
-            })
-
-            it('calls a noop for on', () => {
-                global.window = undefined
-                var emitter = new eventEmitter()
-
-                emitter.on('some new event', () => {})
-                expect(mocks.on).not.to.have.been.called()
-            })
-
-            it('calls a noop for broadcast', () => {
-                global.window = undefined
-                var emitter = new eventEmitter()
-
-                emitter.broadcast('some new event')
-                expect(mocks.emit).not.to.have.been.called()
-            })
-
-            it('calls a noop for broadcast', () => {
-                global.window = undefined
-                var emitter = new eventEmitter()
-
-                emitter.broadcast('some new event')
-                expect(mocks.emit).not.to.have.been.called()
-            })
-            resolve()
-        })
+    it('calls broadcast correctly', (done) => {
+      geb.broadcast('some event', {})
+      expect(mocks.emit).to.have.been.called()
+      mocks.emit.reset()
+      done()
     })
+
+    it('calls on correctly', (done) => {
+      geb.on('some event', () => {})
+      expect(mocks.on).to.have.been.called()
+      mocks.on.reset()
+      done()
+    })
+
+    it('calls once correctly', (done) => {
+      geb.once('some event', () => {})
+      expect(mocks.once).to.have.been.called()
+      mocks.once.reset()
+      done()
+    })
+
+    it('calls off correctly', (done) => {
+      geb.off('some event', () => {})
+      expect(mocks.off).to.have.been.called()
+      mocks.off.reset()
+      done()
+    })
+  })
+
+  describe('server', () => {
+
+    before(async () => {
+      jsdom()
+    })
+
+    return new Promise(resolve => {
+      it('calls a noop for broadcast', () => {
+        //jsdom()
+        var emitter = new eventEmitter({ee})
+
+        emitter.broadcast('some new event')
+        expect(mocks.emit).not.to.have.been.called()
+      })
+
+      it('calls a noop for on', () => {
+        //jsdom()
+        var emitter = new eventEmitter({ee})
+
+        emitter.on('some new event', () => {})
+        expect(mocks.on).not.to.have.been.called()
+      })
+
+      it('calls a noop for broadcast', () => {
+        //jsdom()
+        var emitter = new eventEmitter({ee})
+
+        emitter.broadcast('some new event')
+        expect(mocks.emit).not.to.have.been.called()
+      })
+
+      it('calls a noop for broadcast', () => {
+        //jsdom()
+        var emitter = new eventEmitter({ee})
+
+        emitter.broadcast('some new event')
+        expect(mocks.emit).not.to.have.been.called()
+      })
+      resolve()
+    })
+  })
 })
